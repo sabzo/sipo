6daa1d39038e3a58d6afb05bea04c99cd1668cb4
Sabelo Mhlambi <sabelo@sabelo.io>
2016-01-31 11:32:29 -0800

    Added helper functions for loggin in and resetting user password. Function to retrieve JSON from http request. Added much more comments

--- app.rb
+++ app.rb
@@ -14,6 +14,9 @@ class App < Sinatra::Base
    set :root, File.dirname(__FILE__)
    set :session_secret, "supersecret"
 
+   ###
+   ## SETUP THE AUTHENTICATION for the API
+   ###
    # Added a authentication logic "strategy" to the Warden Middleware
    Warden::Strategies.add(:password) do
      # If this is a POST request let params be the JSON
@@ -27,13 +30,11 @@ class App < Sinatra::Base
        elsif request.get?
          # use params
        end
-
      end
-     # Since request is valid, let's authenticate it
+     ## Since request is valid, let's authenticate it
      def authenticate!
        req = request.body
        u = User.new().find_one({email: @json['email']})
-
        if u.id && ( BCrypt::Password.new(u.password) == @json['password'])
           #.set_user(u.id, scope: :user)
           # Store logged in user
@@ -45,7 +46,7 @@ class App < Sinatra::Base
        # Based on results of u `fail` or `approve the request`
      end
    end
-
+   ## Configure Session and Default Scope
    use Warden::Manager do |config|
      # more info at: https://github.com/hassox/warden/wiki/Setup
      config.default_scope = :user
@@ -57,19 +58,19 @@ class App < Sinatra::Base
      config.serialize_from_session{|email| User.new().find_one( {email: email}) }
      config.failure_app = self # This app will handle any failures
    end
-
-   # Whenever a request fails to load, always do the following
+   ## Whenever a request fails to load, always do the following
    Warden::Manager.before_failure do |env,opts|
      env['REQUEST_METHOD'] = 'POST'
    end
 
-   # for CORS some browsers first send an options request
+   ###
+   ## Set up CORS: Allows other servers to call this API
+   ###
    options "*" do
      response.headers["Allow"] = "HEAD,GET,PUT,POST,DELETE,OPTIONS"
      response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
      200
    end
-
   before do
     # in the future '*' can be www.example.org to only allow users from example.org
     headers 'Access-Control-Allow-Origin' => '*',
@@ -77,54 +78,90 @@ class App < Sinatra::Base
          'Access-Control-Allow-Headers' => ['Content-Type', 'Accept', 'X-Requested-With', 'access_token']
     end
 
-   # Authenticate a user
+    ###
+    ##  Helper Functions
+    ###
+    ## If a request needs to be authenticated call this method
+    private def login()
+      unless env['warden'].authenticated?(:user)
+        env['warden'].authenticate(:password)
+      end
+    end
+    ## Get JSON from request & transform the JSON Object keys into symbols
+    private def getJSONFromRequest()
+      # variable `request` is known by Sinatra to be the HTTP request
+      JSON.parse( request.body.read ).inject({}){|m,(k,v)| m[k.to_sym] = v; m}
+    end
+
+   ###
+   ## Begin API Routes ##
+   ###
+
+   ###
+   ## App main entry point
+   ###
+   get '/' do
+    # HomePage
+    p "APP v 1.0"
+   end
+   ###
+
+   ## Auth routes
+   ###
+   ## Authenticate user and create a session
    post '/auth/login' do
      unless env['warden'].authenticated?(:user)
       env['warden'].authenticate(:password)
      end
      json({ message: "Successfully logged in bruh!"})
    end
-   # Response page when user is unauthenticated
+   ## Response page when user is unauthenticated
    post '/auth/unauthenticated' do
      content_type :json
      json({ message: "Sorry, this request can not be authenticated. Try again." })
    end
-
-    # Log user out!
+   ## Log user out!
     get '/auth/logout' do
       env['warden'].raw_session.inspect
       env['warden'].logout
       redirect '/'
    end
 
-   get '/' do
-    # HomePage
-    p "APP v 1.0"
-   end
-
-   # Get a particular User
-   get '/user/:id' do
-      # First Authenticate before retrieving a user
-      env['warden'].authenticate(:password)
-      json = getJSONFromRequest()
-
-     u = User.new().find_one(
-       {
-         email: json[:email]
-       })
-
-     # really long, but if user was found return the user doc in JSON or return this error message
-     if u.id && ( BCrypt::Password.new(u.password) == json[:password])
+   ###
+   ## USER routes. Create a user, delete a user, modify a user's password etc.,
+   ###
+   ## Get a particular User
+   get '/user' do
+     # First Authenticate before retrieving a user
+     env['warden'].authenticate(:password)
+     @json = getJSONFromRequest()
+     u = User.new().find_one({email: @json[:email]})
+     #if user was found return the user doc in JSON or return this error message
+     if u.id && ( BCrypt::Password.new(u.password) == @json[:password])
        return json(u.doc)
      else
        msg = {message: "The email or password you entered is incorrect"}
        return json(msg)
      end
    end
-   # Create a new user
+   ## Create a new user
    post '/user' do
     json = getJSONFromRequest()
     u = User.new(json).insert()
    end
 
+   ## Reset Password
+   post '/user/reset' do
+     @json = getJSONFromRequest()
+     u = User.new().find_one({ email: @json[:email]})
+     if u.id # if object has an id then 'obviously' it came from the database
+       random_password = Array.new(10).map { (65 + rand(58)).chr }.join
+       # sendgrid, mailchimp, or mail to  send random password
+       # https://github.com/mikel/mail
+       msg = {message: "Sent temporary password to #{@json[:email]}"}
+     else
+      msg = {message: "Unable to reset password for #{ @json[:email]}"}
+     end
+     json(msg)
+   end
  end
